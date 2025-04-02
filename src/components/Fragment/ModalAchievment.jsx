64dee220e253784ef9aa6dacdee46189e4460ae7
mdaffamahendra <daffa123mahendra@gmail.com>
2025-04-02 07:52:55 +0700

    perbaikan modal achievements

--- src/components/Fragment/ModalAchievment.jsx
+++ src/components/Fragment/ModalAchievment.jsx
@@ -8,8 +8,10 @@ const AchievementModal = ({ isOpen, onClose }) => {
 
   const [achievements, setAchievements] = useState([]);
   const token = useSelector((state) => state.users.token);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     const getAchievements = async () => {
       try {
         await getData(`achievements`, token).then((res) => {
@@ -17,6 +19,8 @@ const AchievementModal = ({ isOpen, onClose }) => {
         });
       } catch (error) {
         console.error("Error fetching activity:", error);
+      } finally {
+        setIsLoading(false);
       }
     };
     getAchievements();
@@ -24,8 +28,6 @@ const AchievementModal = ({ isOpen, onClose }) => {
 
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-4">
-      {" "}
-      {/* Perubahan di sini */}
       <div className="bg-white rounded-lg shadow-lg w-full max-w-md max-h-[80vh] overflow-y-auto relative">
         <div className="sticky top-0 bg-white p-4 border-b flex justify-between items-center">
           <h2 className="text-xl font-semibold text-indigo-600 flex items-center">
@@ -39,26 +41,32 @@ const AchievementModal = ({ isOpen, onClose }) => {
           </button>
         </div>
         <div className="p-4 space-y-3">
-          {achievements.map((ach, index) => (
-            <div
-              key={index}
-              className="bg-indigo-50 p-3 rounded-md flex items-start hover:bg-indigo-100 transition-colors"
-            >
-              <Trophy
-                className="text-indigo-500 mr-3 mt-0.5 flex-shrink-0"
-                size={20}
-              />
-              <div>
-                <div className="flex gap-2">
-                  <h3 className="font-semibold text-indigo-700">{ach.name}</h3>
-                  <span className="text-indigo-700">
-                    {ach.obtained && <BadgeCheck size={20} />}
-                  </span>
+          {isLoading ? (
+            <p className="text-indigo-600 text-md">Loading....</p>
+          ) : (
+            achievements.map((ach, index) => (
+              <div
+                key={index}
+                className="bg-indigo-50 p-3 rounded-md flex items-start hover:bg-indigo-100 transition-colors"
+              >
+                <Trophy
+                  className="text-indigo-500 mr-3 mt-0.5 flex-shrink-0"
+                  size={20}
+                />
+                <div>
+                  <div className="flex gap-2">
+                    <h3 className="font-semibold text-indigo-700">
+                      {ach.name}
+                    </h3>
+                    <span className="text-indigo-700">
+                      {ach.obtained && <BadgeCheck size={20} />}
+                    </span>
+                  </div>
+                  <p className="text-sm text-gray-600">{ach.description}</p>
                 </div>
-                <p className="text-sm text-gray-600">{ach.description}</p>
               </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
       </div>
     </div>
